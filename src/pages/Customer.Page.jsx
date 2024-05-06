ad158e51ec10e03e39c0e735eefa812b4a988613
Nahir Sanchez <NahirSanchez@capitalizeconsulting.com>
2024-05-06 14:03:27 -0700

    Fixed search field on customer view

--- src/pages/Customer.Page.jsx
+++ src/pages/Customer.Page.jsx
@@ -98,7 +98,7 @@ function Customer() {
             const countryFilter = customer.shippingAddress.country ? customer.shippingAddress.country.toLowerCase().includes(lowerCaseFilter) : false
             const contactNameFilter = customer.contactName ? customer.contactName.toLowerCase().includes(lowerCaseFilter) : false
             const contactNumberFilter = customer.contactNumber ? customer.contactNumber.toLowerCase().includes(lowerCaseFilter) : false
-            const chineseNameFilter = customer.customFields["18"].value ? customer.customFields["18"].value.toLowerCase().includes(lowerCaseFilter) : false
+            const chineseNameFilter = customer.customFields["18"]?.value ? customer.customFields["18"].value.toLowerCase().includes(lowerCaseFilter) : false
             return (
                 customer.number.toLowerCase().includes(lowerCaseFilter) ||
                 customer.name.toLowerCase().includes(lowerCaseFilter) ||
